ecfd9ff96f28e7a80e24c1f09740d3a043163a6a
Ahmed Walied <2301647@student.eelu.edu.eg>
2026-08-08 16:11:54 +0300

    feat(core): add ScreenUtils and SourceResponse model

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:news_app/ui/home_view.dart';
 import 'package:news_app/utils/app_routes.dart';
+import 'package:news_app/utils/app_theme.dart';
 
 void main() {
   runApp(const NewsApp());
@@ -11,10 +13,18 @@ class NewsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {AppRoutes.homeRouteName: (context) => HomeView()},
-      initialRoute: AppRoutes.homeRouteName,
+    return ScreenUtilInit(
+      designSize: const Size(393, 852),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {AppRoutes.homeRouteName: (context) => HomeView()},
+        initialRoute: AppRoutes.homeRouteName,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: .light,
+      ),
     );
   }
 }
